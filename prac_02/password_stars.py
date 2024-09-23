72bf437f28bb5c6d0df4f9e38c8fa54a269a8768
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-09-23 18:36:59 +0800

    refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,22 @@
-get_user_password = input("Enter your password with at least 10 characters: ")
+def main():
+    get_user_password = user_password()
 
-while len(get_user_password) < 10:
-    print("Your password must contain at least 10 characters.")
+    asterisk_password(get_user_password)
+
+
+def asterisk_password(get_user_password):
+    starred_password = "*" * len(get_user_password)
+    print(starred_password)
+
+
+def user_password():
     get_user_password = input("Enter your password with at least 10 characters: ")
+    while len(get_user_password) < 10:
+        print("Your password must contain at least 10 characters.")
+        get_user_password = input("Enter your password with at least 10 characters: ")
+    return get_user_password
 
-starred_password = "*" * len(get_user_password)
-print(starred_password)
+
+main()
+
+
