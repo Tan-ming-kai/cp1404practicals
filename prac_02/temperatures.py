c9256558b36bf1d2f6add855423ca03241551ffa
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-09-25 14:32:02 +0800

    before putting functions in main

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,38 +1,36 @@
 def main():
+    MENU = """    C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            fahrenheit = convert_to_fahrenheit()
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            celsius = convert_to_celsius()
+            print(f"Result: {celsius:.2f} C")
 
+            # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+            # Remove the "pass" statement when you are done. It's a placeholder.
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
 
 
 def convert_to_fahrenheit():
     get_celsius = float(input("Celsius: "))
-    get_fahrenheit = celsius * 9.0 / 5 + 32
-    return fahrenheit
+    get_fahrenheit = get_celsius * 9.0 / 5 + 32
+    return get_fahrenheit
 
 
 def convert_to_celsius():
     get_fahrenheit = float(input("Fahrenheit: "))
-    get_celsius = 5 / 9 * (fahrenheit - 32)
-    return celsius
+    get_celsius = 5 / 9 * (get_fahrenheit - 32)
+    return get_celsius
 
-
-while choice != "Q":
-    if choice == "C":
-        fahrenheit = convert_to_fahrenheit()
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        convert_to_celsius()
-        celsius = convert_to_celsius()
-        print(f"Result: {celsius:.2f} C")
-
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-    else:
-        print("Invalid option")
-    print(MENU)
-    choice = input(">>> ").upper()
-print("Thank you.")
+main()
