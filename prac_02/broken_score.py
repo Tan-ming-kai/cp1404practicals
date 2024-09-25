6b4cdd3a6fbe0a267e3d58c9ea652aa986fd15d0
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-09-25 14:53:18 +0800

    changed user score into a function

--- prac_02/broken_score.py
+++ prac_02/broken_score.py
@@ -1,20 +1,49 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+# """
+# CP1404/CP5632 - Practical
+# Broken program to determine score status
+# """
+#
+#
+# score = float(input("Enter score: "))
+#
+# if score < 0:
+#     print("Invalid score")
+#
+# else:
+#     if score > 100:
+#         print("Invalid score")
+#     elif score >= 90:
+#         print("Excellent")
+#     elif score >= 50:
+#         print("passable")
+# if score < 50:
+#     print("Bad")
+from unicodedata import category
 
 
-score = float(input("Enter score: "))
 
-if score < 0:
-    print("Invalid score")
+def main():
+    result_comment = get_user_score()
+    print(result_comment)
 
-else:
-    if score > 100:
-        print("Invalid score")
+
+def get_user_score():
+    score = float(input("Enter score: "))
+    if score < 0 or score >100:
+        grade = "Enter a valid score"
+        return grade
+
     elif score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("passable")
-if score < 50:
-    print("Bad")
+        grade = "Excellent"
+        return grade
+
+    elif score >=50:
+        grade = "passable"
+        return grade
+
+    else:
+        grade = "Bad"
+        return grade
+
+
+main()
