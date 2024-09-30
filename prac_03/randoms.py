ab25a4fef87779ed599c83c7213604652329e449
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-09-30 17:06:18 +0800

    randoms.py done

---
+++ prac_03/randoms.py
@@ -0,0 +1,8 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+#Max:20 Min:5
+print(random.randrange(3, 10, 2))  # line 2
+#Max:9 Min:3 unable to produce a 4
+print(random.uniform(2.5, 5.5))  # line 3
+#Max:5.5 Min:2.5
