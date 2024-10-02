2e5ec738cb8f5931329f6ddaad8696e1692d7859
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-10-02 14:58:21 +0800

    added function to check total of file numbers

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,3 +1,4 @@
+from time import process_time_ns
 
 name = input("Enter your name: ")
 
@@ -5,7 +6,11 @@ name = input("Enter your name: ")
 def main():
     name_writer()
     file_reader()
-    numbers_txt_file_reader()
+
+    first_two_numbers = first_two_lines_of_numbers_txt()
+    print(first_two_numbers)
+    total = total_of_numbers_txt()
+    print(total)
 
 
 
@@ -23,14 +28,18 @@ def file_reader():
 
 
 
-def numbers_txt_file_reader():
+def first_two_lines_of_numbers_txt():
     with open("numbers.txt", "r") as read_file:
-        first_two_lines = read_file.readlines()
-        first_two_numbers = int(first_two_lines[0]) + int(first_two_lines[1])
-        print(first_two_numbers)
+        first_two_numbers = int(read_file.readline()) + int(read_file.readline())
+        return first_two_numbers
 
+def total_of_numbers_txt():
+    with open("numbers.txt", "r") as read_file:
+        total = 0
+        for line in read_file:
+            total = total + int(line)
 
-
+        return total
 main()
 
 
