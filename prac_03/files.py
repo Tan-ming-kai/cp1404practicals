5afade784273a67cee57e1a2941255e6e7308da6
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-10-02 15:00:52 +0800

    added docstring

--- prac_03/files.py
+++ prac_03/files.py
@@ -16,12 +16,14 @@ def main():
 
 
 def name_writer():
+    """Writes user input of name to name.txt"""
     in_file = open("name.txt", "w")
     in_file.write(name)
     in_file.close()
 
 
 def file_reader():
+    """Reads name.txt and adds a greeting"""
     in_file = open("name.txt", "r")
     print(f"Hi {in_file.read()}!")
     in_file.close()
@@ -29,11 +31,13 @@ def file_reader():
 
 
 def first_two_lines_of_numbers_txt():
+    """Adds value of first 2 numbers of numbers.txt"""
     with open("numbers.txt", "r") as read_file:
         first_two_numbers = int(read_file.readline()) + int(read_file.readline())
         return first_two_numbers
 
 def total_of_numbers_txt():
+    """Adds total of all numbers in numbers.txt"""
     with open("numbers.txt", "r") as read_file:
         total = 0
         for line in read_file:
