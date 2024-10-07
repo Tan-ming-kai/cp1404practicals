a744dcc46edba681a57b8d48027970c08cc529ed
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-10-07 18:30:49 +0800

    added user name checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,6 +1,7 @@
-
 def main():
     numbers = []
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface','BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer','bob']
+
     numbers = get_user_numbers(numbers)
     print(numbers)
     print(f"The first number is {numbers[0]}")
@@ -8,6 +9,15 @@ def main():
     print(f"The smallest number is {min(numbers)}")
     print(f"The largest number is {max(numbers)}")
     print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+    print()
+
+    username_validity = username_checker(usernames)
+
+    if username_validity:
+        print("username is valid")
+    else:
+        print("username is invalid")
+
 
 
 def get_user_numbers(numbers):
@@ -16,6 +26,20 @@ def get_user_numbers(numbers):
         user_number = int(input("Enter a number: "))
         numbers.append(user_number)
     return numbers
+
+def username_checker(usernames):
+    """checks if the username inputted is present in the list of usernames"""
+    username_validity = False
+    user_username = input("Enter a username: ")
+
+    for username in usernames:
+        if user_username == username:
+            username_validity = True
+            return username_validity
+        else:
+            username_validity = False
+
+    return username_validity
 
 
 
