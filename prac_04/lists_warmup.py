f92775384e3e5bbe6555ba477047b7d6c5789844
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-10-07 16:54:08 +0800

    lists_warmup done

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,1 +1,40 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0]
+#3
+# numbers[-1]
+#2
+# numbers[3]
+#1
+
+# numbers[:-1]
+#[3,1,4,1,5,9]
+
+# numbers[3:4]
+#[1]
+
+# 5 in numbers
+#True
+
+# 7 in numbers
+#False
+
+# "3" in numbers
+#False
+
+# numbers + [6, 5, 3]
+#[3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = "ten"
+print(numbers)
+
+numbers[-1] = 1
+
+for number in range(2,len(numbers)):
+    print(numbers[number])
+
+if 9 in numbers:
+    print("9 is in numbers")
+else:
+    print("9 is not in numbers")
+
