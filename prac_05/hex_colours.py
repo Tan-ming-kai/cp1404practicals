d9ef2717bc25354eb55ff859d0aba5391b4d15d6
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-10-15 22:12:11 +0800

    added dictionary with the hexcodes of the colours

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,5 @@
+HEXCODE_TO_COLOUR = {"ABSOLUTE ZERO": "#0048ba", "ACID GREEN": "#b0bf1a", "ALICEBLUE": "#f0f8ff",
+                     "ALIZARIN CRIMSON": "e32636", "AMARANTH": "#e52b50", "AMBER": "#ffbf00", "AMETHYST": "#9966cc",
+                     "ANTIQUEWHITE": "#faebd7", "APRICOT": "#fbceb1", "AQUA": "#00ffff"}
+
+
