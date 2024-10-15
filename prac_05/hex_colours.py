6a8344967d5418d5d599ca82eb6b7ac429aa1200
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-10-15 22:22:42 +0800

    complete prac 5 hex colours exercise

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,4 +2,11 @@ HEXCODE_TO_COLOUR = {"ABSOLUTE ZERO": "#0048ba", "ACID GREEN": "#b0bf1a", "ALICE
                      "ALIZARIN CRIMSON": "e32636", "AMARANTH": "#e52b50", "AMBER": "#ffbf00", "AMETHYST": "#9966cc",
                      "ANTIQUEWHITE": "#faebd7", "APRICOT": "#fbceb1", "AQUA": "#00ffff"}
 
+colour_choice = input("Enter colour: ").upper()
 
+while colour_choice != "":
+    if colour_choice in HEXCODE_TO_COLOUR:
+        print(f"{colour_choice} --> {HEXCODE_TO_COLOUR[colour_choice]}")
+    else:
+        print("Invalid choice")
+    colour_choice = input("Enter colour: ").upper()
