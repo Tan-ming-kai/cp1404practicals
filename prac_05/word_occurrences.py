bb61c42c059b8d829860650cfbbadef4d3cd090c
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-10-15 22:38:30 +0800

    added code to count the word occurrences in a user text and store in a dictionary

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,3 +3,10 @@ Word occurrences
 Estimate: 15 mins
 Actual:
 """
+word_to_count = {}
+user_text = input("Enter a long text here: ")
+for word in user_text.split():
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+
+print(word_to_count)
+
