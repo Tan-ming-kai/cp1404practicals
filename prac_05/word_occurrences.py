024d1e99385e8f9d5de60490acee428a630a613a
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-10-15 22:45:04 +0800

    added code to format the display of the dictionary

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,10 +3,13 @@ Word occurrences
 Estimate: 15 mins
 Actual:
 """
+from operator import *
 word_to_count = {}
 user_text = input("Enter a long text here: ")
 for word in user_text.split():
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
-print(word_to_count)
+longest_word = max(len(word) for word in list(word_to_count))
+for word,count in sorted(word_to_count.items()):
+    print(f"{word:{longest_word+1}}: {count}")
 
