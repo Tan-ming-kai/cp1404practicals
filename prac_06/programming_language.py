7e7a447757ecdcb19f2f2ecf5038e4147c81dcff
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-10-31 20:31:54 +0800

    changed language to language_name for readability

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,19 +1,19 @@
 class ProgrammingLanguages:
 
-    def __init__(self, language="", typing="", reflection="", year=0):
-        self.language = language
+    def __init__(self, language_name="", typing="", reflection="", year=0):
+        self.language_name = language_name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
-        return f""
+        return f"{self.language_name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def __repr__(self):
         return self.__str__()
 
     def is_dynamic(self):
-        if self.typing == "DYNAMIC":
+        if self.typing == "Dynamic":
             return True
         else:
             return False
