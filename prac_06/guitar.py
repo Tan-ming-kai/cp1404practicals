8df4309b7a4145789bac11a5a23b6b0867fc51a5
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-10-31 20:39:15 +0800

    added init, str, repr, get_age, is_vintage methods to guitar.py class

---
+++ prac_06/guitar.py
@@ -0,0 +1,22 @@
+class Guitar:
+    def __init__(self, name="", year=0, cost=0.0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : {self.cost}"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def get_age(self):
+        guitar_age = 2024 - self.year
+        return guitar_age
+
+    def is_vintage(self, guitar_age):
+        guitar_age = self.get_age()
+        if guitar_age >= 50:
+            return True
+        else:
+            return False
