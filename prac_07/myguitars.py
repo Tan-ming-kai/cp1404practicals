7579adf0748bc8788a13dd7d2b5ad7070a1b2dad
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-11-04 17:52:58 +0800

    completed myguitars.py code

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -3,6 +3,8 @@ from prac_07.guitar import Guitar
 
 def main():
     guitars = reads_guitar_csv_file()
+
+    #sorts the guitars list with the __lt__ method
     guitars.sort()
 
     for guitar in guitars:
@@ -13,6 +15,8 @@ def reads_guitar_csv_file():
     """reads guitar.csv file and returns it into a list"""
     guitars = []
     in_file = open('guitars.csv', 'r')
+
+    # stores each line as a separate object of the Guitar class
     for line in in_file:
         parts = line.strip().split(',')
         guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
