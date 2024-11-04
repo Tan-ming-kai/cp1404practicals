048b48e8a203a5be2a8026e4cb980e6dfb9f9bbe
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-11-04 18:39:20 +0800

    completed myguitars.py

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -4,7 +4,7 @@ from prac_07.guitar import Guitar
 def main():
     guitars = reads_guitar_csv_file()
 
-    #sorts the guitars list with the __lt__ method
+    # sorts the guitars list with the __lt__ method
     guitars.sort()
 
     for guitar in guitars:
@@ -12,9 +12,14 @@ def main():
 
     # adding a new guitar
     user_guitar_name = input("Name of your guitar: ")
-    user_guitar_year = input("Year of your guitar: ")
-    user_guitar_cost = input("Cost of your guitar: ")
-    user_guitar = Guitar(user_guitar_name, user_guitar_year,user_guitar_cost)
+    user_guitar_year = int(input("Year of your guitar: "))
+    user_guitar_cost = float(input("Cost of your guitar: "))
+    user_guitar = [Guitar(user_guitar_name, user_guitar_year, user_guitar_cost)]
+    guitars.append(user_guitar)
+    guitars.sort()
+
+    with open("guitars.csv", "a") as guitars_file:
+        print(f"{user_guitar_name},{user_guitar_year},{user_guitar_cost}", file=guitars_file)
 
 
 def reads_guitar_csv_file():
@@ -25,11 +30,10 @@ def reads_guitar_csv_file():
     # stores each line as a separate object of the Guitar class
     for line in in_file:
         parts = line.strip().split(',')
-        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+        guitar = [Guitar(parts[0], int(parts[1]), float(parts[2]))]
         guitars.append(guitar)
     in_file.close()
     return guitars
-
 
 
 main()
