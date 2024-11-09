79920cde6c24914d5a4fb13592991f0d7b6de334
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-11-09 13:08:11 +0800

    i did not change anything but commiting to get rid of it in commit page

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -5,11 +5,8 @@ class Guitar:
         self.year = year
         self.cost = cost
 
-    def __str__(self):
-        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
-
     def __repr__(self):
-        return self.__str__()
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
 
     def __lt__(self, other):
         return self.year < other.year
@@ -24,4 +21,5 @@ class Guitar:
             return True
         else:
             return False
+
 
