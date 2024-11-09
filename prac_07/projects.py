80fa227fb12ccfb3654ffd33d40896a8f93688ae
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-11-09 17:59:48 +0800

    completed project_management.py and projects.py with docstring and comments yet to do

--- prac_07/projects.py
+++ prac_07/projects.py
@@ -1,8 +1,10 @@
+import datetime
+
 
 class Project:
-    def __init__(self, name="", date="", priority=0, cost_estimate=0.0, percentage=0):
+    def __init__(self, name, start_date, priority=0, cost_estimate=0.0, percentage=0):
         self.name = name
-        self.date = date
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.percentage = percentage
@@ -16,9 +18,11 @@ class Project:
     def updated_percentage(self, updated_percentage):
         self.percentage = updated_percentage
 
+    def __lt__(self, other):
+        return self.priority < other.priority
+
     def __str__(self):
-        return f"{self.name}, start: {self.date}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.percentage}%"
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.percentage}%"
 
     def __repr__(self):
         return self.__str__()
-
