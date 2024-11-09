cc0b6c6256eb9a2aa4a93a4230fe0e27ab1b5b94
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-11-09 13:29:07 +0800

    changed the menu function as it was not displaying properly and added the display projects function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -12,29 +12,44 @@ menu = """Menu:
 - (F)ilter projects by date
 - (A)dd new project  
 - (U)pdate project
-- (Q)uit
-"""
+- (Q)uit"""
 
 print("Welcome to Pythonic Project Management")
+
+
 def main():
-
-
     projects = extract_file_information()
     print(f"Loaded {len(projects)} projects from projects.txt")
 
     print(menu)
-    user_choice = ""
+    user_choice = input(">>> ").upper()
+
     while user_choice != 'Q':
-        user_choice = input("Choose an option: ").strip().upper()
 
         if user_choice == 'L':
             print()
         elif user_choice == 'S':
             print()
         elif user_choice == 'D':
-            print("Incomplete Projects")
+
+            completed_projects = []
+            incomplete_projects = []
             for project in projects:
+                if project.project_is_completed():
+                    completed_projects.append(project)
+                else:
+                    incomplete_projects.append(project)
+
+            print("Incomplete Projects")
+            for project in incomplete_projects:
                 print(f"\t{project}")
+
+            print("Completed Projects")
+            for project in completed_projects:
+                print(f"\t{project}")
+            # blank line after display function
+            print()
+
         elif user_choice == 'F':
             print()
         elif user_choice == 'A':
@@ -43,12 +58,14 @@ def main():
             print()
         else:
             print(f"{user_choice} is not a valid option")
-            # end of program
+        print(menu)
+        user_choice = input(">>> ").upper()
+
     print("Thank you for using custom-built project management software.")
 
 
-
 def extract_file_information():
+    """ Reads file information and stores in project_list"""
     project_list = []
     with open("projects.txt", "r") as in_file:
         in_file.readline()
@@ -58,6 +75,4 @@ def extract_file_information():
             project_list.append(project)
         return project_list
 
-
 main()
-
