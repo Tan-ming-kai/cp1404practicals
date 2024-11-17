49be8bafb157f521d1fc5ff135a84bed111d3550
tanmingkai <mingkai.tan@my.jcu.edu.au>
2024-11-17 14:01:58 +0800

    added docstrings to functions

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -6,14 +6,18 @@ from kivy.properties import StringProperty
 MILES_TO_KILOMETERS = 1.6
 
 class MilesToKm(App):
+    """kivy app to change miles to km"""
     message = StringProperty()
     """ MilesToKm App changes miles to kilometers """
+
     def build(self):
+        """build kivy GUI"""
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def converts_distance(self, value):
+        """converts the distance from miles to KM"""
         try:
             result = float(value) / MILES_TO_KILOMETERS
             self.root.ids.output_label.text = str(result)
@@ -21,9 +25,11 @@ class MilesToKm(App):
             self.root.ids.output_label.text = "0"
 
     def handles_output(self, message):
+        """sets the output label to a string """
         self.root.ids.output_label.text = message
 
     def handle_increment(self, value):
+        """either increase or decrease the user value by 1 based on up or down"""
         new_value = int(self.root.ids.miles_input.text) + value
         self.root.ids.miles_input.text = str(new_value)
         self.converts_distance(new_value)
